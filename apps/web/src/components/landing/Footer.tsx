2b6b743bd2f1529f18ffc323956de0e2aef81cb1
deeg454 <diego.medina@my.utsa.edu>
2026-05-02 18:52:22 -0500

    last fix before i lose it

--- apps/web/src/components/landing/Footer.tsx
+++ apps/web/src/components/landing/Footer.tsx
@@ -56,7 +56,7 @@ export default function Footer() {
 						</Link>
 						<Link href="https://go.rowdyhacks.org/discord">
 							<Image
-								className="select-none invert"
+								className="select-none "
 								src="/img/assets/discord_icon.svg"
 								alt="Discord logo"
 								width={20}
